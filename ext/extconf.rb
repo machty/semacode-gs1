a4d6d8fa85c81617aabd4cf37f7c555d11160f48
Gregory McIntyre <greg@gregorymcintyre.com>
2015-07-24 10:46:06 +1000

    Get rid of warnings.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,3 +1,11 @@
 require 'mkmf' 
-dir_config("semacode_native")
-create_makefile('semacode_native')
+dir_config 'semacode_native'
+if CONFIG['CC'] =~ /gcc/
+  $CFLAGS << ' ' + %w[
+    -Wno-sign-compare -Wno-pointer-sign
+    -Wno-incompatible-pointer-types-discards-qualifiers
+    -Wno-shorten-64-to-32 -Wno-missing-braces
+    -Wno-format-extra-args -Wno-char-subscripts
+  ].join(' ').strip
+end
+create_makefile 'semacode_native'
